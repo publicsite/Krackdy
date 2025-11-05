90a3e0ea01fe40cd49c542f568e6c5994030d04e
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-05 23:44:06 +0000

    Update stage2.sh Set umask

--- stage2.sh
+++ stage2.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #stage2 :- debootstraps a vanilla rootfs for the appropriate architecture
 
+OLD_UMASK="$(umask)"
+umask 0022
+
 #enter directory containing this script
 cd $(dirname $(realpath $0))
 
@@ -132,3 +135,6 @@ su kodi -c "/workdir/getEquiptmentBuild.sh /workdir"
 umount /proc
 umount /sys
 umount /dev/pts
+
+umask "${OLD_UMASK}"
+
